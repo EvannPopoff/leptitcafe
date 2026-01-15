045b31fd2879a1d3c6703f70f957843adf6ff000
Negin <neginsn.studio@gmail.com>
2026-01-15 16:58:14 +0100

    SAE3-41: Ajout structure de base page adhésion

---
+++ app/views/pages/membership.php
@@ -0,0 +1,7 @@
+<!-- Page Adhésion -->
+<main>
+    <section class="hero-adhesion">
+        <h1>ADHÉRER & SOUTENIR</h1>
+        <p>Devenez acteur de votre café associatif</p>
+    </section>
+</main>
