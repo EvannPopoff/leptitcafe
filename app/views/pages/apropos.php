abcf41193859fd8e45e383eda2fdf9452ca21745
Olgaevm <Volha.YAUMENAVA@etu.uca.fr>
2026-01-15 16:33:57 +0100

    timeline

--- app/views/pages/apropos.php
+++ app/views/pages/apropos.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Page A propos</title>
-        <link rel="stylesheet" href="assets/css/style.css">
-    </head>
- <body>
             <div class="hero">
             <img src="assets/images/image_header.webp" alt="hero">
         </div>
@@ -58,4 +48,3 @@
             </div>
     </div>
 </section>
-</html>
