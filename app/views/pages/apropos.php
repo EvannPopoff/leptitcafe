f5884e21d8bb8696000c2ad68d6ef59a67f43622
Olgaevm <Volha.YAUMENAVA@etu.uca.fr>
2026-01-15 14:54:40 +0100

    page a propos

--- app/views/pages/apropos.php
+++ app/views/pages/apropos.php
@@ -5,11 +5,10 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Page d'Accueil</title>
+        <title>Page A propos</title>
 
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
- <link rel="stylesheet" href="/assets/css/style.css">
  <body>
             <div class="hero">
             <img src="assets/images/image_header.webp" alt="hero">
