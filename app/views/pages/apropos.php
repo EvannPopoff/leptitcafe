3d35518d563211c6939f7b9b0a748def58027a03
Olgaevm <Volha.YAUMENAVA@etu.uca.fr>
2026-01-15 14:51:51 +0100

    images page apropos

--- app/views/pages/apropos.php
+++ app/views/pages/apropos.php
@@ -1,6 +1,18 @@
- <link rel="stylesheet" href="/leptitcafe/assets/css/style.css">
+ <!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page d'Accueil</title>
+
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
  <body>
             <div class="hero">
-            <img src="/leptitcafe/assets/images/image_header.webp" alt="hero">
+            <img src="assets/images/image_header.webp" alt="hero">
         </div>
 </body>
+
+
+</html>
