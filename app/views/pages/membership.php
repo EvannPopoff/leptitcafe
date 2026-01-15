686338a13cdbbc0062c700c914c4c76308f24441
Negin <neginsn.studio@gmail.com>
2026-01-15 17:27:24 +0100

    Ajout section pourquoi adherer

--- app/views/pages/membership.php
+++ app/views/pages/membership.php
@@ -4,4 +4,10 @@
         <h1>ADHÉRER & SOUTENIR</h1>
         <p>Devenez acteur de votre café associatif</p>
     </section>
+ <section class="pourquoi-adherer">
+    <div class="container">
+        <h2>Pourquoi adhérer à l'association ?</h2>
+           <p>Pour profiter du lieu et de nos activités, recevoir le programme et soutenir l'Association Jeunes Pousses.</p>
+        </div>
+    </section>
 </main>
