f3adc1753329082dc9b9dbe9497aae9b980501a4
EvannPopoff <evann.popoff@gmail.com>
2026-01-16 08:58:44 +0100

    merge branche backend

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Footer – Le P'tit Café</title>
-
-  <link rel="stylesheet" href="assets/css/style.css">
-</head>
-
-<body>
-
 <footer class="site-footer">
   <div class="footer-top">
 
@@ -19,28 +7,22 @@
 
     <div class="footer-column">
       <h3>Adresse</h3>
-      <p>
-        25 Place du Marché Couvert<br>
-        43000 Le Puy-en-Velay
-      </p>
+      <p>25 Place du Marché Couvert<br>43000 Le Puy-en-Velay</p>
     </div>
 
     <div class="footer-column">
       <h3>Contact</h3>
-      <p>
-        07 78 68 53 37<br>
-        cafejeunespousses[at]gmail.com
-      </p>
+      <p>07 78 68 53 37<br>cafejeunespousses[at]gmail.com</p>
     </div>
 
     <div class="footer-column">
       <h3>Pages</h3>
       <ul>
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="app/views/pages/apropos.php">À propos</a></li>
-        <li><a href="app/views/pages/evenement">Activités et événements</a></li>
-        <li><a href="app/views/pages/adherer">Adhérer</a></li>
-        <li><a href="app/views/pages/contact">Contact</a></li>
+        <li><a href="index.php?page=apropos">À propos</a></li>
+        <li><a href="index.php?page=evenement">Activités et événements</a></li>
+        <li><a href="index.php?page=membership">Adhérer</a></li>
+        <li><a href="index.php?page=contact">Contact</a></li>
       </ul>
     </div>
 
@@ -61,5 +43,4 @@
   </div>
 </footer>
 
-</body>
-</html>
+<script src="assets/js/header.js"></script>
--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -5,7 +5,7 @@
         <img src="assets/images/img-hero.webp" alt="hero">
 
         <div class="hero-content">
-            <h1>LE P’TIT CAFÉ</h1>
+            <h1>Le P’tit Café</h1>
             <p>
                 Un café associatif au Puy-en-Velay,  
                 où chacun trouve sa place !<br>
@@ -15,6 +15,43 @@
             <a href="apropos.php" class="btn-hero">En savoir plus</a>
         </div>
     </div>
-        
+
+    <section class="events">
+        <div class="events-container">
+
+            <h2>Événements phares</h2>
+            <p class="events-subtitle">
+            Retrouvez les événements organisés par le P’tit Café.
+            </p>
+
+            <div class="events-list">
+
+            <article class="event-card">
+                <img src="assets/images/event1.avif" alt="Calendrier de l’Après">
+                <h3>Calendrier de l’Après</h3>
+                <p>
+                Une programmation culturelle et artistique, offerte pendant les 24 jours avant Noël.</p>
+            </article>
+
+            <article class="event-card">
+                <img src="assets/images/event2.avif" alt="Festin Nomade">
+                <h3>Festin Nomade</h3>
+                <p>
+                Le Festin Nomade s’installe le samedi 15 octobre pour une journée festive, succulente et familiale.</p>
+            </article>
+
+            <article class="event-card">
+                <img src="assets/images/event3.avif" alt="Quinzaine des Droits de l’enfant">
+                <h3>Quinzaine des Droits de l’Enfant</h3>
+                <p>
+                Ces ateliers permettent de mettre en lumière les droits de l’enfant auprès du grand public.</p>
+            </article>
+
+        </div>
+
+        <a href="#" class="btn-events">Découvrir plus</a>
+
+    </section>
+
 
 </body>
