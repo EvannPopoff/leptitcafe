d8be0b4f57f87d4f0907f017c295efda840fa60d
Negin <neginsn.studio@gmail.com>
2026-01-18 00:54:56 +0100

    Ajout section formules adhesion avec carte individuelle

--- app/views/pages/membership.php
+++ app/views/pages/membership.php
@@ -10,4 +10,15 @@
            <p>Pour profiter du lieu et de nos activités, recevoir le programme et soutenir l'Association Jeunes Pousses.</p>
         </div>
     </section>
+ <h2>Nos formules d'adhésion annuelles</h2>
+            
+            <div class="cards-container">
+                <div class="card-adhesion">
+                    <h3>Individuelle</h3>
+                    <p class="price">7€ / an</p>
+                    <a href="#" class="btn-choisir">Choisir</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
