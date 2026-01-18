176f9d2854119ad6df37e2472514b2a10a5bfefe
Negin <neginsn.studio@gmail.com>
2026-01-18 21:15:25 +0100

    Ajout formulaire connexion

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -3,3 +3,13 @@
     <div class="login-container">
         <h1>CONNEXION</h1>
         <p>Veuillez vous identifier pour accéder au tableau de bord.</p>
+        <form class="login-form">
+            <div class="form-group">
+                <label>Adresse email</label>
+                <input type="email" placeholder="Email">
+        </div>
+            <div class="form-group">
+                <label>Mot de passe</label>
+                <input type="password" placeholder="Mot de passe">
+            </div>
+        </form>
