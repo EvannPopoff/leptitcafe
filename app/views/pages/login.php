f4499a24a1771f6383fd39a7d171d1cfd417bc5b
Negin <neginsn.studio@gmail.com>
2026-01-18 21:01:32 +0100

    Creation page connexion admin

---
+++ app/views/pages/login.php
@@ -0,0 +1,5 @@
+<!-- Page Connexion Admin -->
+<section class="login-section">
+    <div class="login-container">
+        <h1>CONNEXION</h1>
+        <p>Veuillez vous identifier pour accéder au tableau de bord.</p>
